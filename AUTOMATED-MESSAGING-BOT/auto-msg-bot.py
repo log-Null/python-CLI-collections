caba0213ae5b3a94d2d4922da4a0610645d17982
log-Null <paweethera@gmail.com>
2025-09-13 14:09:03 +0530

    Create auto-msg-bot.py

---
+++ AUTOMATED-MESSAGING-BOT/auto-msg-bot.py
@@ -0,0 +1,45 @@
+import pyautogui, time, datetime
+
+time.sleep(2)
+while True:
+  
+
+    print(datetime.datetime.now())
+    pyautogui.typewrite("Peek-a-boo! I see you 👁️") 
+    pyautogui.press("enter")
+    time.sleep(5)
+
+    print(datetime.datetime.now())
+
+    pyautogui.typewrite("Back. Again. You’re welcome")
+    pyautogui.press("enter")
+    time.sleep(5)
+
+    print(datetime.datetime.now())
+
+    pyautogui.typewrite("Yes, yes… I know you missed me 😏")
+    pyautogui.press("enter")
+    time.sleep(5)
+    
+    pyautogui.typewrite("Oops… did I pop up again?")
+    pyautogui.press("enter")
+    time.sleep(5)
+
+    print(datetime.datetime.now())
+
+    pyautogui.typewrite("Peek-a-boo! I see you 👁️")
+    pyautogui.press("enter")
+    time.sleep(5)
+
+    print(datetime.datetime.now())
+
+    pyautogui.typewrite("Back. Again. You’re welcome")
+    pyautogui.press("enter")
+    time.sleep(5)
+
+    print(datetime.datetime.now())
+
+    pyautogui.typewrite("Yes, yes… I know you missed me 😏")
+    pyautogui.press("enter")
+    time.sleep(5)
+    
